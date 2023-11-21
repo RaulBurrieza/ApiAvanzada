f230b18f46ab9fbb0567ef6a49ba633c33dacd5b
manulis <manulisdesable@gmail.com>
2023-11-21 09:57:11 +0100

    GET tables

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List, Optional
@@ -6,47 +8,76 @@ from supabase import create_client, Client
 import psycopg2 as psyc
 import uvicorn as uvi
 import asyncio
+import json
+import hashlib as hlb
+import requests as rq
+from fastapi.responses import RedirectResponse
 
 #Modelos
-class User(BaseModel):
+class Usuario(BaseModel):
     id:Optional[str]
-    name:Optional[str]
-    password:Optional[str]
-    
+    NombreUsu:Optional[str]
+    Contraseña:Optional[str]
+
+class infoUsuario(BaseModel):
+    Nombre:str
+    Email:str
+    Telefono:str
+    Fecha_nacimiento:str
+     
 app = FastAPI(
     title="Api Avanzada"
 )
+
+
+navInit ="start chrome http://127.0.0.1:5000"
+
 #Conexiones
 URL = "https://jpztuzgyiluqazttymmb.supabase.co"
 KEY =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwenR1emd5aWx1cWF6dHR5bW1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY4NTI4NTcsImV4cCI6MjAxMjQyODg1N30.R1ppxbGbX0pJ2rDysdlqdJ3QXiDvvjOtV-d5WuepWVQ"
 sp: Client = create_client(URL,KEY)
 conn = psyc.connect(host = "db.jpztuzgyiluqazttymmb.supabase.co",port="5432",database="postgres",user="postgres",password= "qlJb4WrwJc5UdzF1")
 cur = conn.cursor()
+
+#Arrays
 Tablas = []
+GetTable = []
 
 cur.execute("""SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'""")
 for table in cur.fetchall():  
-    print(table)
-    Tablas.append(str(table).replace("'", "").replace(",", "").replace("(", "").replace(")", "")) 
+    Tablas.append(str(table).replace("'", "").replace(",", "").replace("(", "").replace(")", ""))
+       
+def initUvicorn():
+    async def main():
+        config = uvi.Config("main:app", port=5000, log_level="info")
+        server = uvi.Server(config)
+        await server.serve()
 
-async def main():
-    config = uvi.Config("main:app", port=5000, log_level="info")
-    server = uvi.Server(config)
-    await server.serve()
+    if __name__ == "__main__":
+        asyncio.run(main())
+        
+    @app.get('/', include_in_schema=False)
+    async def docs_redirect():
+        response = RedirectResponse(url='/docs')
+        return response
 
-if __name__ == "__main__":
-    asyncio.run(main())
+    #operaciones GET
 
-#CRUD
-@app.get('/')
-def greeting():
-    return "hola"
-
-@app.get('/tables')
-def getTables():
-    return Tablas
-
-@app.get('/users')
-def getUsers():
+    @app.get('/tables')
+    def getTables():
+        return Tablas
     
-    return "hola"
+    @app.get('/tables/{id}')
+    def getTablaInfo(id:int):
+            data = sp.table(Tablas[id]).select("*").execute()
+            tableInfo = data.model_dump_json()
+            jsonUsers = json.loads(tableInfo)
+            return jsonUsers
+        
+    #operaciones POST
+        
+
+    
+initUvicorn()
+
+proceso = subprocess.Popen(navInit, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
